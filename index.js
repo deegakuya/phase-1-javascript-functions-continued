dfa56250dfe19bd20de11c6cb690df1249ba7f71
Diana Gakuya <deegakuya@gmail.com>
2023-09-19 23:27:46 +0300

    Assignment 2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// Define the function saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define the function mondayWork
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Define the function wrapAdjective
+  function wrapAdjective(flair = "*") {
+    return function (adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+
+  
+  
